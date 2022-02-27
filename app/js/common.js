c6ad35b0e225a74876e7f124846ba48d9d91b5f3
Viktoria <zaharova181114@gmail.com>
2022-02-27 22:26:15 +0200

    768

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.reviews-slider').slick({
+    slidesToShow: 2,
+    dots: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow.png" alt=""></button>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
